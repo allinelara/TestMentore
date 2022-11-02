c7a233f70687d429a6bb7f8815bdd962ea56cb03
allinelara <allinelara@gmail.com>
2022-11-02 10:01:42 +0100

    implementing remote data sync

--- network/src/main/java/com/allinedelara/network/RemoteDataSync.kt
+++ network/src/main/java/com/allinedelara/network/RemoteDataSync.kt
@@ -1,25 +1,71 @@
 package com.allinedelara.network
 
 import com.allinedelara.core.DataSync
+import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.FormBody
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
-import java.io.IOException
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+
 
 abstract class RemoteDataSync <T, K>(val okHttpClient: OkHttpClient): DataSync<T,K> {
     abstract fun getUrl():String
-    abstract fun getUrlList():String
     override suspend fun put(item: T) {
          withContext(Dispatchers.IO) {
+             val url = getUrl()
+             val jsonItem: String = Gson().toJson(item)
              val mediaType = "application/json; charset=utf-8".toMediaType()
 
-             val url = getUrl()
-            val body: RequestBody = RequestBody.
-            val request = Request.Builder().put(body).url(url).build()
+             val requestBody = jsonItem.toRequestBody(mediaType)
+
+             val request: Request = Request.Builder()
+                 .url(url)
+                 .put(requestBody)
+                 .build()
+             try {
+                 okHttpClient.newCall(request).execute()
+             }catch (io: IOException){
+                 null
+             }
+        }
+    }
+
+    override suspend fun putList(listItems: List<T>) {
+        withContext(Dispatchers.IO) {
+            val url = getUrl()
+            val jsonItem: String = Gson().toJson(listItems)
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+
+            val requestBody = jsonItem.toRequestBody(mediaType)
+
+            val request: Request = Request.Builder()
+                .url(url)
+                .put(requestBody)
+                .build()
+            try {
+                okHttpClient.newCall(request).execute()
+            }catch (io: IOException){
+                null
+            }
+        }
+    }
+
+    override suspend fun delete(id: K) {
+        withContext(Dispatchers.IO) {
+            val url = getUrl()
+            val jsonItem: String = Gson().toJson(id)
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+
+            val requestBody = jsonItem.toRequestBody(mediaType)
+
+            val request: Request = Request.Builder()
+                .url(url).delete(requestBody)
+                .build()
             try {
                 okHttpClient.newCall(request).execute()
             }catch (io: IOException){

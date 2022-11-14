cca0eb6f36eb7b1d8b84073fa276f6bfac25340a
allinelara <allinelara@gmail.com>
2022-11-14 20:23:00 +0100

    added  new tests

--- network/src/test/java/com/allinedelara/network/RemoteDataSourceTest.kt
+++ network/src/test/java/com/allinedelara/network/RemoteDataSourceTest.kt
@@ -1,13 +1,12 @@
 package com.allinedelara.network
 
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
 import kotlinx.coroutines.runBlocking
 import okhttp3.*
+import okio.BufferedSource
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import java.io.IOException
 import java.io.InputStream
 
 class RemoteDataSourceTest{
@@ -55,17 +54,21 @@ class RemoteDataSourceTest{
     @org.junit.jupiter.api.Test
     fun testGet(){
         //given
-        val fakeURl = "jijio"
+        val fakeURl = "https://test.com/"
         val fakeId = "oi"
         val mockInputStream : InputStream = mock()
         val mockBody = mock<ResponseBody>()
+        val mockBuffer: BufferedSource = mock()
         val mockResponse = mockResponse(200, "test",mockBody )
         val mockCall : Call = mock()
         val fakeItem = "item"
         whenever(mockSource.getUrl(fakeId)).doReturn(fakeURl)
         whenever(mockSource.parseSingleItem(mockInputStream)).doReturn(fakeItem)
-        doReturn(mockInputStream).whenever(mockBody).byteStream()
-        whenever(mockHttpClient.newCall(any())).doReturn(mockCall)
+        whenever(mockBody.source()).doReturn(mockBuffer)
+        whenever(mockBuffer.inputStream()).doReturn(mockInputStream)
+        whenever(mockHttpClient.newCall(argThat{
+            this.url.toString() == fakeURl
+        })).doReturn(mockCall)
         whenever(mockCall.execute()).doReturn(mockResponse)
 
     //when
@@ -76,4 +79,155 @@ class RemoteDataSourceTest{
     //then
         assertThat(results).isEqualTo(fakeItem)
     }
+
+    @org.junit.jupiter.api.Test
+    fun testGetNetworkFail(){
+        //given
+        val fakeURl = "https://test.com/"
+        val fakeId = "oi"
+        val mockCall : Call = mock()
+        whenever(mockSource.getUrl(fakeId)).doReturn(fakeURl)
+        whenever(mockHttpClient.newCall(argThat{
+            this.url.toString() == fakeURl
+        })).doReturn(mockCall)
+        whenever(mockCall.execute()).doThrow(IOException())
+
+        //when
+        val results = runBlocking {
+            testedSource.get(fakeId)
+        }
+
+        //then
+        assertThat(results).isNull()
+    }
+
+    //write a test that be returns 400
+    @org.junit.jupiter.api.Test
+    fun testGetReturning400(){
+        //given
+        val fakeURl = "https://test.com/"
+        val fakeId = "oi"
+        val mockInputStream : InputStream = mock()
+        val mockBody = mock<ResponseBody>()
+        val mockBuffer: BufferedSource = mock()
+        val mockResponse = mockResponse(400, "test",mockBody )
+        val mockCall : Call = mock()
+        val fakeItem = "item"
+        whenever(mockSource.getUrl(fakeId)).doReturn(fakeURl)
+        whenever(mockSource.parseSingleItem(mockInputStream)).doReturn(fakeItem)
+        whenever(mockBody.source()).doReturn(mockBuffer)
+        whenever(mockBuffer.inputStream()).doReturn(mockInputStream)
+        whenever(mockHttpClient.newCall(argThat{
+            this.url.toString() == fakeURl
+        })).doReturn(mockCall)
+        whenever(mockCall.execute()).doReturn(mockResponse)
+        whenever(mockCall.execute().code == 400).doThrow(IOException())
+
+
+        //when
+        val results = runBlocking {
+            testedSource.get(fakeId)
+        }
+
+        //then
+        assertThat(results).isNull()
+    }
+
+    //write a test what happen with the parsing fail
+//    @org.junit.jupiter.api.Test
+//    fun testGetFailingParse(){
+//        //given
+//        val mockInputStream : InputStream = mock()
+//        val mockBuffer: BufferedSource = mock()
+//        whenever(mockBuffer.inputStream()).(mockInputStream)
+//        whenever(mockInputStream).doThrow(IOException())
+//
+//        //when
+//        val results = runBlocking {
+//            testedSource.parseList(mockInputStream)
+//        }
+//
+//        //then
+//        assertThat(results).isNull()
+//    }
+
+    @org.junit.jupiter.api.Test
+    fun testGetAll(){
+        //given
+        val fakeURl = "https://test.com/"
+        val mockInputStream : InputStream = mock()
+        val mockBody = mock<ResponseBody>()
+        val mockBuffer: BufferedSource = mock()
+        val mockResponse = mockResponse(200, "test",mockBody )
+        val mockCall : Call = mock()
+        val fakeItens = listOf("item1, item2, item3")
+        whenever(mockSource.getUrlList()).doReturn(fakeURl)
+        whenever(mockSource.parseList(mockInputStream)).doReturn(fakeItens)
+        whenever(mockBody.source()).doReturn(mockBuffer)
+        whenever(mockBuffer.inputStream()).doReturn(mockInputStream)
+        whenever(mockHttpClient.newCall(argThat{
+            this.url.toString() == fakeURl
+        })).doReturn(mockCall)
+        whenever(mockCall.execute()).doReturn(mockResponse)
+
+        //when
+        val results = runBlocking {
+            testedSource.getAll()
+        }
+
+        //then
+        assertThat(results).isEqualTo(fakeItens)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun testGetAllNetworkFail(){
+        //given
+        val fakeURl = "https://test.com/"
+        val mockCall : Call = mock()
+        whenever(mockSource.getUrlList()).doReturn(fakeURl)
+        whenever(mockHttpClient.newCall(argThat{
+            this.url.toString() == fakeURl
+        })).doReturn(mockCall)
+        whenever(mockCall.execute()).doThrow(IOException())
+
+        //when
+        val results = runBlocking {
+            testedSource.getAll()
+        }
+
+        //then
+        assertThat(results).isEqualTo(emptyList<String>())
+    }
+
+    //write a test that be returns 400
+    @org.junit.jupiter.api.Test
+    fun testGetAllReturning400(){
+        //given
+        val fakeURl = "https://test.com/"
+        val mockInputStream : InputStream = mock()
+        val mockBody = mock<ResponseBody>()
+        val mockBuffer: BufferedSource = mock()
+        val mockResponse = mockResponse(400, "test",mockBody )
+        val mockCall : Call = mock()
+        val fakeItens = listOf("item1, item2, item3")
+        whenever(mockSource.getUrlList()).doReturn(fakeURl)
+        whenever(mockSource.parseList(mockInputStream)).doReturn(fakeItens)
+        whenever(mockBody.source()).doReturn(mockBuffer)
+        whenever(mockBuffer.inputStream()).doReturn(mockInputStream)
+        whenever(mockHttpClient.newCall(argThat{
+            this.url.toString() == fakeURl
+        })).doReturn(mockCall)
+        whenever(mockCall.execute()).doReturn(mockResponse)
+        whenever(mockCall.execute().code == 400).doThrow(IOException())
+
+
+        //when
+        val results = runBlocking {
+            testedSource.getAll()
+        }
+
+        //then
+        assertThat(results).isEqualTo(emptyList<String>())
+    }
+
 }

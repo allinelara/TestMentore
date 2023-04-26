6be9466efec335e7b6f8b80e6fae2678bbaf4d70
allinelara <allinelara@gmail.com>
2023-04-26 20:17:35 +0200

    added tests

--- network/src/test/java/com/allinedelara/network/RemoteDataSyncTest.kt
+++ network/src/test/java/com/allinedelara/network/RemoteDataSyncTest.kt
@@ -1,13 +1,11 @@
 package com.allinedelara.network
 
-import com.nhaarman.mockitokotlin2.argThat
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
 import kotlinx.coroutines.runBlocking
 import okhttp3.*
-import org.assertj.core.api.Assertions
+import okio.Buffer
 import org.junit.jupiter.api.BeforeEach
+import java.nio.charset.Charset
 
 class RemoteDataSyncTest {
 
@@ -34,66 +32,76 @@ class RemoteDataSyncTest {
         val mockCall : Call = mock()
         whenever(mockSource.getUrl()).doReturn(fakeURl)
         whenever(mockHttpClient.newCall(argThat{
-            this.url.toString() == fakeURl
+            val correctUrl = this.url.toString() == fakeURl
+            val correctBody = getBody(body) == "\"$fakeItem\""
+            correctUrl && correctBody
         })).doReturn(mockCall)
 
         //when
-        val results = runBlocking {
+        runBlocking {
             testedSource.put(fakeItem)
         }
 
         //then
-        Assertions.assertThat(results).isNotNull
+       verify(mockCall).execute()
     }
 
     @org.junit.jupiter.api.Test
     fun testDelete(){
         //given
         val fakeURl = "https://test.com/"
-        val fakeId = "1"
+        val fakeItem = "1"
         val mockCall : Call = mock()
         whenever(mockSource.getUrl()).doReturn(fakeURl)
         whenever(mockHttpClient.newCall(argThat{
-            this.url.toString() == fakeURl
+            val correctUrl = this.url.toString() == fakeURl
+            val correctBody = getBody(body) == "\"$fakeItem\""
+            correctUrl && correctBody
         })).doReturn(mockCall)
 
         //when
-        val results = runBlocking {
-            testedSource.delete(fakeId)
+        runBlocking {
+            testedSource.delete(fakeItem)
         }
 
         //then
-        Assertions.assertThat(results).isNotNull
+        verify(mockCall).execute()
     }
 
     @org.junit.jupiter.api.Test
     fun testPutList(){
         //given
         val fakeURl = "https://test.com/"
-        val fakeItem = arrayListOf<String>("item", "item2")
+        val fakeItems = listOf("item1", "item2", "item3")
         val mockCall : Call = mock()
         whenever(mockSource.getUrl()).doReturn(fakeURl)
         whenever(mockHttpClient.newCall(argThat{
-            this.url.toString() == fakeURl
+            val correctUrl = this.url.toString() == fakeURl
+            val correctBody = getBody(body) == "\"$fakeItems\""
+            correctUrl && correctBody
         })).doReturn(mockCall)
 
         //when
-        val results = runBlocking {
-            testedSource.putList(fakeItem)
+        runBlocking {
+            testedSource.putList(fakeItems)
         }
 
         //then
-        Assertions.assertThat(results).isNotNull
+        verify(mockCall).execute()
     }
+}
 
-    private fun mockResponse(statusCode: Int, message: String): Response {
-        val fakeResponse = Response.Builder()
-            .request(Request.Builder().url("https://test.com").get().build())
-            .code(statusCode)
-            .message(message)
-            .protocol(Protocol.HTTP_2)
-            //.body(body)
-            .build()
-        return fakeResponse
+private fun getBody(body: RequestBody?):String?{
+    return if(body == null){
+        null
+    }else{
+        try {
+            val sync = Buffer()
+            val charSet = Charset.defaultCharset()
+            body.writeTo(sync)
+            sync.readString(charSet)
+        }catch (e: Exception){
+            null
+        }
     }
 }
